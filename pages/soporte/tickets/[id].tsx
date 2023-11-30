c633f1b588a60d63bb44f6d0e547b2a389612c86
Lautaro Ghosn <lghosn@fi.uba.ar>
2023-11-30 12:08:29 -0300

    update ticketss

--- pages/soporte/tickets/[id].tsx
+++ pages/soporte/tickets/[id].tsx
@@ -1,8 +1,6 @@
 import { ActionButton } from "@/components/actionButton";
 import InputElement from "@/components/inputElement";
 import Loading from "@/components/loading";
-import { MainButton } from "@/components/mainButton";
-import Notification from "@/components/notification";
 import SelectElement from "@/components/selectElement";
 import { SideBar } from "@/components/sideBar";
 import { supportSideBarItems } from "@/utils/routes";
@@ -14,15 +12,15 @@ import { setElementInnerHtml, setElementValue, setSelectValue } from "@/utils/ut
 export default function Ticket() {
   const [loading, setLoading] = useState(true)
   const [ticket, setTicket] = useState([])
-  const [responsables, setResponsables] = useState([])
-  const [notification, setNotification] = useState(false)
+  const [empleados, setEmpleados] = useState([])
   const [responsablesAsignados, setResponsablesAsignados] = useState([])
+  const [responsables, setResponsables] = useState([])
   const router = useRouter();
   const { id } = router.query;
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   function setupData() {
-  // @ts-ignore
+    // @ts-ignore
     setElementInnerHtml("title", ticket.title);
     // @ts-ignore
     setElementValue("description", ticket.description);
@@ -42,6 +40,22 @@ export default function Ticket() {
     setElementValue("creationDate", ticket.creationDate);
   }
 
+  function setupResponsables(){
+    const listResponsables = document.getElementById("responsables")
+    for (let i = 0; i < responsables.length; i++) {
+      // @ts-ignore
+      for (let j = 0; j < listResponsables.length; j++) {
+        // @ts-ignore
+        if (responsables[i].legajo == listResponsables.options[j].value){
+          // @ts-ignore
+          listResponsables.options[j].selected = true
+          // @ts-ignore
+          listResponsables.options[0].selected = false
+        }
+      }
+    }
+  }
+
   useEffect(() => {
     if (id) {
       fetch(`https://psa-support-management.onrender.com/tickets/${id}`)
@@ -51,23 +65,38 @@ export default function Ticket() {
         setTicket(res)
         setLoading(false)
       })
+
+      fetch(`https://psa-support-management.onrender.com/tickets/${id}/responsible`)
+      .then((res) => {
+        return res.json()
+      }).then((res) => {
+        setResponsables(res)
+      })
     }
   }, [id])
 
   useEffect(() => {
     if (!loading) {
+      setupData()
       fetch("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos")
       .then(response => response.json())
       .then(data => {
-        const nuevosResponsables = data.map((item: { legajo: any; Apellido: any; Nombre: any; }) => ({
+        const employees = data.map((item: { legajo: any; Apellido: any; Nombre: any; }) => ({
           value: item.legajo,
           label: `${item.Apellido}, ${item.Nombre}`
         }));
-        setResponsables(nuevosResponsables)
-      }).then(() => setupData())
+        setEmpleados(employees)
+      })
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading])
+
+  useEffect(() => {
+    if (empleados.length > 0) {
+      setupResponsables()
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [empleados])
 
   function deleteTicket() {
     fetch(`https://psa-support-management.onrender.com/tickets/${id}`, {
@@ -78,7 +107,6 @@ export default function Ticket() {
   }
 
   function cancelDelete() {
-    setNotification(false)
     setLoading(true)
     //recargo la pagina
     router.reload()
@@ -91,21 +119,24 @@ export default function Ticket() {
     const severity = document.getElementById("severity").value
     // @ts-ignore
     const priority = document.getElementById("priority").value
-    const responsable = {}
+    const responsable = []
     for (let i = 0; i < responsablesAsignados.length; i++) {
       // @ts-ignore
-      const legajo = responsablesAsignados[i].split(',')[0]
+      const split = responsablesAsignados[i].split(',')
       // @ts-ignore
-      const nombre = responsablesAsignados[i].split(',')[1].replace(/^\s+|\s+$/g, '')
+      const legajo = split[0]
       // @ts-ignore
-      const apellido = responsablesAsignados[i].split(',')[2]
+      const apellido = split[1].replace(/^\s+|\s+$/g, '')
+      // @ts-ignore
+      const nombre = split[2].replace(/^\s+|\s+$/g, '')
       // @ts-ignore
       responsable[i] = {
         "legajo": legajo,
         "firstName": nombre,
-        "lastname": apellido
+        "lastName": apellido
       }
     }
+    console.log(responsable)
 
     fetch(`https://psa-support-management.onrender.com/tickets/${id}/updateFields?newState=${state}&newSeverity=${severity}&newPriority=${priority}`, {
       method: 'PATCH',
@@ -116,15 +147,14 @@ export default function Ticket() {
     })
 
     fetch(`https://psa-support-management.onrender.com/tickets/${id}/assignResponsible`, {
-      method: 'PUT',
+      method: 'POST',
       headers: {
         'accept': '*/*',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(responsable)
-    }).then((res) => {
-      // router.reload()
     })
+    router.reload()
   }
 
   // @ts-ignore
@@ -140,9 +170,6 @@ export default function Ticket() {
     <div className="container max-w-2xl mx-auto mt-8">
       {
       loading ? <div className="flex flex-row justify-center"> <Loading /> </div> :
-      notification ? 
-        <Notification deleteTicket={deleteTicket} cancelDelete={cancelDelete} accion="borrar" />
-      :
      <>
       <div className="flex flex-col border-4 border-gray-500 w-full h-11/12 p-9 mt-20 mb-10">
         <h1 className="text-4xl mb-5 font-bold text-black text-center" id="title" ></h1>
@@ -151,7 +178,7 @@ export default function Ticket() {
           <SelectElement title="Estado" id="state" options={stateData} multiple={false}/>
           <SelectElement title="Prioridad" id="priority" options={priorityData} multiple={false}/>
           <SelectElement title="Severidad" id="severity" options={severityData} multiple={false}/>
-          <SelectElement title="Responsable" id="responsables" options={responsables} multiple onChange={handleSelectChange}/>
+          <SelectElement title="Responsable" id="responsables" options={empleados} multiple onChange={handleSelectChange}/>
         </div>
         <div>
           <InputElement title="Tipo de problema" id="typeOfProblem" type="text" readonly/>
@@ -164,7 +191,6 @@ export default function Ticket() {
       </div>
       <div className="flex flex-row justify-center space-x-10">
         <ActionButton onClick={() => router.back()} title="Volver" />
-        <ActionButton onClick={() => setNotification(true)} title="Borrar" style="bg-red-400 dark:hover:bg-red-700" />
         <ActionButton title="Guardar" onClick={guardarTicket}/>
       </div>
       </>
