425a9cc383ce54895b9127fef1873ce2cc020c3b
Lautaro Ghosn <lghosn@fi.uba.ar>
2023-12-02 01:41:36 -0300

    fix create ticket tarea

--- pages/soporte/tareas.tsx
+++ pages/soporte/tareas.tsx
@@ -43,10 +43,10 @@ export default function TareasTicket() {
       loadingAsignadas ? <div className="flex flex-row justify-center"> <Loading /> </div>:
       <>
       <div className="sm:flex sm:flex-row-reverse mr-16 ml-16 justify-between">
-        <ButtonActionTaskTicket style={' bg-blue-400 hover:bg-blue-600 '} title="Crear tarea" ticketId={""} />
+        <ButtonActionTaskTicket style={' bg-blue-400 hover:bg-blue-600 '} title="Crear tarea" ticketId={ticketId} />
           <button  type="button" onClick={() => router.back()} className="flex flex-row text-gray-500 bg-white hover:bg-gray-100 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-lg  text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-arrow-left-circle mr-2" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+            <path  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
           </svg>
           <div className="text-base">Volver</div>
         </button>
